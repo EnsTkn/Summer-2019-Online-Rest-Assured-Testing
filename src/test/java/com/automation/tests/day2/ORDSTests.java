1f63712418bf829d4aed3bd067d596543b9a07fa
EnsTkn <ente4343@gmail.com>
2020-01-23 11:40:30 -0500

    uptade

---
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -0,0 +1,15 @@
+package com.automation.tests.day2;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSTests {
+
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+
+}
